e253025c309b72323aef141f4022c508e15c281d
Mahdi Kalhor <mkal1375@gmail.com>
2018-08-13 12:40:37 +0430

    Now urls don't have any duplicate!

--- miner.py
+++ miner.py
@@ -6,20 +6,20 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from mysql import connector
 
-from setting import setting
+from setting import settings
 
-
-def save(home_url):
+def read_urls():
     try:
         # Edit setting.py if you need!
-        db = connector.connect(**setting['mysql'])
-        cr = db.cursor()
-        query = 'INSERT INTO {} VALUES("{}")'.format(setting['urls_table'], home_url)
+        db = connector.connect(**settings['mysql'])
+        cr = db.cursor(buffered=True)
+        query = 'SELECT * FROM {};'.format(settings['urls_table'])
         try:
             cr.execute(query)
             db.commit()
+            return [url[0] for url in cr.fetchall()]
         except Exception as e:
-            print("i can't insert <> into <> table :( \n".format(home_url, setting['urls_table'],))
+            print("i can't select urls form <{}> table :( \n".format(settings['urls_table'], ))
             print(e)
             db.rollback()
             cr.close()
@@ -28,12 +28,41 @@ def save(home_url):
             exit()
 
     except Exception as e:
-        print("i can't connect to <{}> database :( \n".format(setting['mysql']['database']))
+        print("i can't connect to <{}> database :( \n".format(settings['mysql']['database']))
+        print(e)
+        sleep(5)
+        exit()
+
+
+
+def save(home_url):
+    try:
+        # Edit setting.py if you need!
+        db = connector.connect(**settings['mysql'])
+        cr = db.cursor()
+        query = 'INSERT INTO {} VALUES("{}")'.format(settings['urls_table'], home_url)
+        try:
+            cr.execute(query)
+            db.commit()
+            cr.close()
+            db.close()
+        except Exception as e:
+            print("i can't insert <> into <> table :( \n".format(home_url, settings['urls_table'], ))
+            print(e)
+            db.rollback()
+            cr.close()
+            db.close()
+            sleep(5)
+            exit()
+
+    except Exception as e:
+        print("i can't connect to <{}> database :( \n".format(settings['mysql']['database']))
         print(e)
         sleep(5)
         exit()
 
 def main():
+    old_links = read_urls()
     browser = webdriver.Firefox()
     browser.get('https://divar.ir/tehran/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/browse/%D8%A7%D8%AC%D8%A7%D8%B1%D9%87-%D9%85%D8%B3%DA%A9%D9%88%D9%86%DB%8C-%D8%A2%D9%BE%D8%A7%D8%B1%D8%AA%D9%85%D8%A7%D9%86-%D8%AE%D8%A7%D9%86%D9%87-%D8%B2%D9%85%DB%8C%D9%86/%D8%A7%D9%85%D9%84%D8%A7%DA%A9-%D9%85%D8%B3%DA%A9%D9%86/')
 
@@ -47,5 +76,7 @@ def main():
         home_link = home_link['href']
         home_link = re.search(r'\/([\w\-]+)$', home_link).group(1)
         home_link = urljoin('http://divar.ir/v/', home_link)
-        save(home_link)
+        if home_link not in old_links:
+            save(home_link)
+
 main()
--- setting.py
+++ setting.py
@@ -1,4 +1,4 @@
-setting = {
+settings = {
     'mysql':{'user': 'root', 'password': '12345678', 'host':'127.0.0.1', 'database': 'hpp'},
     'urls_table': 'urls'
 }
