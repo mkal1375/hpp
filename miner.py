242e08f9e84819d201bf1f079bf341480e8f8452
Mahdi Kalhor <mkal1375@gmail.com>
2018-08-13 12:08:38 +0430

    Change url method for saving storage. Add .idea to .gitignore

--- miner.py
+++ miner.py
@@ -1,18 +1,20 @@
-from selenium import webdriver
+import re
 from time import sleep
-from bs4 import BeautifulSoup as bs
-from mysql import connector
-from setting import setting
 from urllib.parse import urljoin
 
-MAIN_URL = 'https://divar.ir/'
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from mysql import connector
+
+from setting import setting
+
+
 def save(home_url):
     try:
         # Edit setting.py if you need!
         db = connector.connect(**setting['mysql'])
         cr = db.cursor()
         query = 'INSERT INTO {} VALUES("{}")'.format(setting['urls_table'], home_url)
-        print(query)
         try:
             cr.execute(query)
             db.commit()
@@ -35,13 +37,15 @@ def main():
     browser = webdriver.Firefox()
     browser.get('https://divar.ir/tehran/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/browse/%D8%A7%D8%AC%D8%A7%D8%B1%D9%87-%D9%85%D8%B3%DA%A9%D9%88%D9%86%DB%8C-%D8%A2%D9%BE%D8%A7%D8%B1%D8%AA%D9%85%D8%A7%D9%86-%D8%AE%D8%A7%D9%86%D9%87-%D8%B2%D9%85%DB%8C%D9%86/%D8%A7%D9%85%D9%84%D8%A7%DA%A9-%D9%85%D8%B3%DA%A9%D9%86/')
 
-    for i in range(2): # 5 times = 3 page (48 home)
+    for i in range(2): # 5 times = 3 page (3 * 48 home)
         browser.execute_script('window.scrollTo(0,document.body.scrollHeight);')
         sleep(1)
     page_source = browser.page_source
     soup = bs(page_source, 'html.parser')
     homes_links = soup.select('.post-card-link')
-    homes_links = [urljoin(MAIN_URL,home_link['href']) for home_link in homes_links]
-    for home in homes_links:
-        save(home)
+    for home_link in homes_links:
+        home_link = home_link['href']
+        home_link = re.search(r'\/([\w\-]+)$', home_link).group(1)
+        home_link = urljoin('http://divar.ir/v/', home_link)
+        save(home_link)
 main()
